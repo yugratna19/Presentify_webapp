a377bcc0f36919ffb93848459be6a0bdb69de6e5
Yugratna Humagain <yugratna59@gmail.com>
2024-02-29 17:11:47 +0545

    [feat] path file

---
+++ path.py
@@ -0,0 +1,7 @@
+#yugratna path
+temp_path = r"C:\Users\ACER\Desktop\Presentify_webapp\temp_pdf.pdf"
+slides_path = r"C:\Users\ACER\Desktop\Presentify_webapp\slides"
+display_path = r"C:\Users\ACER\Desktop\Presentify_webapp\display"
+image_path = r"C:\Users\ACER\Desktop\Presentify_webapp\images"
+
+#add path and comment others if wanted
